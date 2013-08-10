a16429c471a5ff3eeec90750babbc0c88d8f336e
CarlosPrada <carlos_8878@hotmail.com>
2013-08-10 15:39:32 -0500

    Cambios en QuienesSomos, Eventos y Menú

--- WebScience/src/java/uis/giib/portal/controlador/EventosPortalController.java
+++ WebScience/src/java/uis/giib/portal/controlador/EventosPortalController.java
@@ -84,62 +84,7 @@ public class EventosPortalController implements Serializable {
 
         return calendar;
     }
-
-    /*
-    private Date previousDay8Pm() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.PM);
-    t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
-    t.set(Calendar.HOUR, 8);
-    return t.getTime();
-    }
-    private Date previousDay11Pm() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.PM);
-    t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
-    t.set(Calendar.HOUR, 11);
-    return t.getTime();
-    }
-    private Date today1Pm() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.PM);
-    t.set(Calendar.HOUR, 1);
-    return t.getTime();
-    }
-    private Date theDayAfter3Pm() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);
-    t.set(Calendar.AM_PM, Calendar.PM);
-    t.set(Calendar.HOUR, 3);
-    return t.getTime();
-    }
-    private Date today6Pm() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.PM);
-    t.set(Calendar.HOUR, 6);
-    return t.getTime();
-    }
-    private Date nextDay9Am() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.AM);
-    t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
-    t.set(Calendar.HOUR, 9);
-    return t.getTime();
-    }
-    private Date nextDay11Am() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.AM);
-    t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
-    t.set(Calendar.HOUR, 11);
-    return t.getTime();
-    }
-    private Date fourDaysLater3pm() {
-    Calendar t = (Calendar) today().clone();
-    t.set(Calendar.AM_PM, Calendar.PM);
-    t.set(Calendar.DATE, t.get(Calendar.DATE) + 4);
-    t.set(Calendar.HOUR, 3);
-    return t.getTime();
-    }*/
+    
     public List<Publicacion> getListadoPublicacionEvento() {
         return listadoPublicacionEvento;
     }
